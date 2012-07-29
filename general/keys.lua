3f8c4b88c1c7b748320f438aa007f24823fea0a4
Tim Roes <mail@timroes.de>
2012-07-29 18:30:49 +0200

    Added multimedia keys

--- general/keys.lua
+++ general/keys.lua
@@ -25,10 +25,18 @@ shortcuts = awful.util.table.join(
 	awful.key({ modkey }, "d", function() debug("Msg") end),
 
 	-- Multimedia keys
-	--awful.key({ }, "XF86AudioRaiseVolume",    function () awful.util.spawn("amixer set Master 2+") end),
-	--awful.key({ }, "XF86AudioLowerVolume",    function () awful.util.spawn("amixer set Master 2-") end),
-	--awful.key({ }, "XF86AudioMute",    function () awful.util.spawn("amixer set Master toggle") end)
-	--
+	awful.key({ }, "XF86AudioPlay", function() awful.util.spawn("mpc toggle -q") end),
+	awful.key({ }, "XF86AudioNext", function() awful.util.spawn("mpc next -q") end),
+	awful.key({ }, "XF86AudioPrev", function() awful.util.spawn("mpc prev -q") end),
+	awful.key({ modkey }, "XF86AudioPlay", function() 
+		local song = awful.util.pread("mpc current"):sub(1,-2)
+		if song:len() == 0 then
+			song = "- nothing playing -"
+		end
+		naughty.notify({ text = song, title = "Currently playing:", timeout = 3 })
+	end),
+
+	-- Grun launcher
 	awful.key({ modkey }, "space", function() awful.util.spawn("grun") end)
 )
 
