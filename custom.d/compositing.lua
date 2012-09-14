9ae4df3c352e5e3dba9b39fea03b6b031b7b8767
Tim Roes <mail@timroes.de>
2012-09-14 20:01:11 +0200

    Check for class not nil

--- custom.d/compositing.lua
+++ custom.d/compositing.lua
@@ -12,7 +12,7 @@ end)
 -- Start compositing when last wine window is closed
 client.add_signal("unmanage", function(c)
 
-	if c.class:lower() ~= "wine" then
+	if not c.class or c.class:lower() ~= "wine" then
 		return
 	end
 
